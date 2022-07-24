0e23606f563069e4a93669080c551019fd3a3e26
Vinicius Isumi <pancake.tf2@gmail.com>
2022-07-24 01:05:39 -0300

    Iniciado tela de dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,7 @@
 const Dashboard = () => {
-  return <div>DASHBOARD</div>;
+  return <div className='h-screen bg-purple-900 flex'>
+    <div className='bg-white h-2/3 w-2/3 mx-auto flex my-auto rounded-lg'></div>
+  </div>;
 };
 
 export default Dashboard;
