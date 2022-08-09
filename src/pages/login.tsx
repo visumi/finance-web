f314a8e9a60699a32f7eab9917931bdd5d7a44fd
Vinicius Isumi <pancake.tf2@gmail.com>
2022-08-09 20:31:44 -0300

    Corrigido mt login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -17,7 +17,7 @@ const Login = () => {
 
   return (
     <div className='flex flex-col w-full p-12 gap-8 h-screen bg-no-repeat bg-cover bg-poly'>
-      <div className='flex justify-center gap-2 px-4 py-2 mt- mx-auto bg-purple-800 rounded-full shadow-md left-1/2'>
+      <div className='flex justify-center gap-2 px-4 py-2 mx-auto bg-purple-800 rounded-full shadow-md left-1/2'>
       <WarningCircle className='my-auto text-purple-50' size={20} weight="bold" />
         <span className='text-purple-100'>
           Projeto desenvolvido apenas para aprendizado, não forneça dados reais.
